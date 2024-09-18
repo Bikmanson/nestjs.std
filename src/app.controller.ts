19b456f4519571980e80db7d01827668f88b756d
vbikman <vbikman@mindk.com>
2024-09-18 11:38:04 +0300

    v.1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
@@ -9,4 +10,24 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+=======
+import {Controller, Get, Param, Post, Query} from '@nestjs/common';
+
+import {CacheService, CacheKey, CacheValue, CacheSetResponse} from "@lib/cache";
+
+@Controller()
+export class AppController {
+    constructor(private readonly cache: CacheService) {
+    }
+
+    @Get('cache/:key')
+    getCache(@Param('key') key: CacheKey): Promise<CacheValue> {
+        return this.cache.get(key);
+    }
+
+    @Post('cache')
+    addToCache(@Query('key') key: CacheKey, @Query('value') value: CacheValue): Promise<CacheSetResponse> {
+        return this.cache.set(key, value);
+    }
+>>>>>>> 976f9379 (first commit)
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,5 +7,27 @@ import { AppService } from './app.service';
   imports: [],
   controllers: [AppController],
   providers: [AppService],
+=======
+import {Module} from '@nestjs/common';
+
+import {CacheModule} from "@lib/cache";
+
+import {AppController} from '@app/app.controller';
+import {ConfigModule} from "@nestjs/config";
+
+@Module({
+    imports: [
+        ConfigModule.forRoot(),
+        CacheModule.forRoot({
+            host: process.env.REDIS_HOST,
+            port: Number(process.env.REDIS_PORT),
+        }),
+        CacheModule.forRootAsync({
+            host: process.env.REDIS_HOST,
+            port: Number(process.env.REDIS_PORT),
+        }),
+    ],
+    controllers: [AppController],
+>>>>>>> 976f9379 (first commit)
 })
 export class AppModule {}
